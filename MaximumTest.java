eb3d3294e13ac621ef825c130cd2044e543dc664
PoojaSolankar <poojasolankar09gmail.com>
2022-03-26 00:10:17 +0530

    Using Generic Test Case

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,21 +1,21 @@
 package com.Bridgelabz.Generic;
 
-public class MaximumTest {
-		String x, y, z;
+public class MaximumTest<T extends Comparable<T>> {
+		T x, y, z;
 	
-	public MaximumTest(String x, String y, String z) {
-		super();
+	public MaximumTest(T x, T y, T z) {
+		//super();
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
-	public String maximum() {
+	public T maximum() {
 		return MaximumTest.maximum(x, y, z);
 	}
 	
-	public static String maximum(String x, String y, String z) {
-		String max = x;
+	public static <T extends Comparable<T>>T maximum(T x, T y, T z) {
+		T max = x;
 		if(y.compareTo(max)>0) {
 			max = y;
 		}
@@ -38,14 +38,21 @@ public class MaximumTest {
 		return max;
 	}
 	
-	public static void printMax(String x, String y, String z, String max) {
+	public static <T> void printMax(T x, T y, T z, T max) {
 		System.out.println(x +" "+ y +" "+ z + "\nMaximum no is : "+ max);
 	}
 	
 	public static void main(String[] args) {
+		Integer xInt = 2, yInt = 4, zInt = 7;
+		Float xFlt = 1.1f, yFlt = 2.2f, zFlt = 3.3f;
 		String xStr = "Parrot", yStr = "Peacoke", zStr ="Cat";
 		
 		MaximumTest.testMaximum(xStr, yStr, zStr);
+		System.out.println("________________________");
+		new MaximumTest(xInt, yInt, zInt).maximum();
+		System.out.println("________________________");
+		new MaximumTest(xFlt, yFlt, zFlt).maximum();
+		System.out.println("________________________");
 		new MaximumTest(xStr, yStr, zStr).maximum();
 	}
 }
